8b3409f0fb981e506151235590e9ccb7e55e0a03
github username <github email address>
2018-01-10 18:36:06 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,67 +1,57 @@
-// import React from 'react';
-//
-// class MovieReviews extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//
-//
-//   render() {
-//     let reviews;
-//     reviews = this.props.reviews.map(review => {
-//       return <div className="review">
-//         <h3>{review.display_title}</h3>
-//       </div>
-//     })
-//
-//     return (
-//       <div className="review-list">{reviews}</div>
-//     )
-//   }
-// }
-//
-// MovieReviews.defaultProps = {
-// reviews: [],
-// };
-//
-//
-// export default MovieReviews;
-
-
 import React from 'react';
 
-const Review = ({
-  headline,
-  byline,
-  link,
-  summary_short
-}) => {
-  return (
-
-    <div
-      key={headline}
-      className="review"
-    >
-      <header>
-        <a
-          className="review-link"
-
-          href={link.url}
-        >
-          {headline}
-        </a>
-        <br/>
-        <span className="author">{byline}</span>
-      </header>
-      <blockquote>{summary_short}</blockquote>
-    </div>
-  );
-};
-
-const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+const MovieReviews = ({reviews}) =>  (
+  <div className="review-list">
+    {reviews.map(review => (
+      <div className="review">
+      {review.display_title}
+      </div>
+    ))}
+  </div>
+)
 
 MovieReviews.defaultProps = {
-  reviews: []
+reviews: [],
 };
 
+
 export default MovieReviews;
+
+//
+// import React from 'react';
+//
+// const Review = ({
+//   headline,
+//   byline,
+//   link,
+//   summary_short
+// }) => {
+//   return (
+//
+//     <div
+//       key={headline}
+//       className="review"
+//     >
+//       <header>
+//         <a
+//           className="review-link"
+//
+//           href={link.url}
+//         >
+//           {headline}
+//         </a>
+//         <br/>
+//         <span className="author">{byline}</span>
+//       </header>
+//       <blockquote>{summary_short}</blockquote>
+//     </div>
+//   );
+// };
+//
+// const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+//
+// MovieReviews.defaultProps = {
+//   reviews: []
+// };
+//
+// export default MovieReviews;
