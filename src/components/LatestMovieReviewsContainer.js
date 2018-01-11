d96e8cc1fa5c011a59e1955d5a13c490950cb6cc
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2018-01-11 08:22:00 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'ee3618493d8b455aad2d378e3888d7c8';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component{
+  constructor(props){
+    super();
+
+    this.state = {
+      reviews: []
+    }
+}
+componentDidMount() {
+    fetch(`${URL}`)
+    .then(resp=>resp.json())
+    .then(json=>this.setState({ reviews: json.results }, () => console.log(this.state)))
+  }
+
+  render(){
+    return(
+      <div className="latest-movie-reviews">
+          {this.state.reviews.map(movie => <div className="movie" /*key={movie.display_title}*/>
+          <h3>{movie.display_title}</h3>
+          <p>Byline:{movie.byline}</p>
+          <p>Critics pick:{movie.critics_pick}</p>
+          <p>Updated date:{movie.date_updated}</p>
+          <a href={movie.link.url}>{movie.link.suggested_link_text}</a>
+          <br/>
+          <img src={movie.multimedia.src} alt=""></img>
+          <p>Opening date:{movie.opening_date}</p>
+          <p>Summary:{movie.summary_short}</p>
+
+          </div>)}
+      </div>
+    )
+  }
+}
+
+
+
+
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,17 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react'
+
+class SearchableMovieReviewsContainer extends React.Component{
+  constructor(){
+    super();
+
+  }
+  render(){
+    return(
+      "Hi"
+    )
+  }
+// fetch(`${URL}`).then(resp=>console.log(resp))
+}
+
+export default SearchableMovieReviewsContainer
