2777d9c87b5c22f28314da210dcda32e4a39b9ab
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2018-01-11 17:42:25 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'ee3618493d8b455aad2d378e3888d7c8';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
@@ -8,7 +9,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 // Code LatestMovieReviewsContainer Here
 class LatestMovieReviewsContainer extends React.Component{
   constructor(props){
-    super();
+    super(props);
 
     this.state = {
       reviews: []
@@ -23,18 +24,7 @@ componentDidMount() {
   render(){
     return(
       <div className="latest-movie-reviews">
-          {this.state.reviews.map(movie => <div className="movie" /*key={movie.display_title}*/>
-          <h3>{movie.display_title}</h3>
-          <p>Byline:{movie.byline}</p>
-          <p>Critics pick:{movie.critics_pick}</p>
-          <p>Updated date:{movie.date_updated}</p>
-          <a href={movie.link.url}>{movie.link.suggested_link_text}</a>
-          <br/>
-          <img src={movie.multimedia.src} alt=""></img>
-          <p>Opening date:{movie.opening_date}</p>
-          <p>Summary:{movie.summary_short}</p>
-
-          </div>)}
+      <MovieReviews reviews = {this.state.reviews}/>
       </div>
     )
   }
