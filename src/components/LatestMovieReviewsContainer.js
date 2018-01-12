5956f14ad441b85a3cac0c2c6ec76fdf94e7b502
c0nniewang <connie.wang303@gmail.com>
2018-01-12 10:04:58 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'ee6ee6c24164424d8f2142d18eea507a';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${URL}`)
+    .then(resp => resp.json())
+    .then(json => this.setState({
+      reviews: json.results}))
+  }
+
+  render() {
+    return <div className="latest-movie-reviews">
+    <MovieReviews reviews={this.state.reviews} />
+    </div>
+  }
+} 
+
+export default LatestMovieReviewsContainer;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
 
+
 ReactDOM.render(
   <div className="app">
     <SearchableMovieReviewsContainer />
